6c24f59fa51ecc22b420c871684fc51b7994795b
foggynight <foggynight709@gmail.com>
2020-12-14 23:34:09 -0330

    SDL: Add cell arrays

---
+++ SDL/src/engine.c
@@ -0,0 +1,4 @@
+#include "engine.h"
+
+static char curr_cells[CELL_ARRAY_WIDTH * CELL_ARRAY_HEIGHT];
+static char past_cells[CELL_ARRAY_WIDTH * CELL_ARRAY_HEIGHT];
---
+++ SDL/src/engine.h
@@ -0,0 +1,7 @@
+#ifndef ENGINE_H_
+#define ENGINE_H_
+
+#define CELL_ARRAY_WIDTH  640
+#define CELL_ARRAY_HEIGHT 480
+
+#endif
