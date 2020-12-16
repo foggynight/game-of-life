03e162979819cfb85bd12ffe42e8271e4daef4ca
foggynight <foggynight709@gmail.com>
2020-12-16 16:19:29 -0330

    SDL: Add CELL_COUNT

--- SDL/src/engine.c
+++ SDL/src/engine.c
@@ -1,4 +1,4 @@
 #include "engine.h"
 
-static char curr_cells[CELL_ARRAY_WIDTH * CELL_ARRAY_HEIGHT];
-static char past_cells[CELL_ARRAY_WIDTH * CELL_ARRAY_HEIGHT];
+static char curr_cells[CELL_COUNT];
+static char past_cells[CELL_COUNT];
