e9aceeb0c4384bdd0bdeaeb836e8fb846afee772
foggynight <foggynight709@gmail.com>
2020-12-19 18:26:00 -0330

    SDL: Add neighbours to each cell

--- SDL/src/engine.c
+++ SDL/src/engine.c
@@ -11,22 +11,57 @@ static cell_t cell_arr[CELL_COUNT];
 void engine_setup(void)
 {
     /* Add neighbours to each cell */
-    for (int i = 0; i < CELL_COUNT; ++i) {
-        /* Column to the left */
-        if (i%CELL_ARRAY_WIDTH > 0) {
+    for (int i = 0, ni; i < CELL_COUNT; ++i) {
+        cell_t *targ = &cell_arr[i]; // Target cell
 
+        /* Cell above */
+        if (i >= CELL_ARRAY_WIDTH) {
+            ni = i - CELL_ARRAY_WIDTH;
+            cell_add_neighbour(targ, &cell_arr[ni]);
         }
+
+        /* Cell below */
+        else if (i < CELL_ARRAY_WIDTH * (CELL_ARRAY_HEIGHT-1)) {
+            ni = i + CELL_ARRAY_WIDTH;
+            cell_add_neighbour(targ, &cell_arr[ni]);
+        }
+
+        /* Column to the left */
+        else if (i%CELL_ARRAY_WIDTH > 0) {
+            /* Cell above to the left */
+            if (i >= CELL_ARRAY_WIDTH) {
+                ni = i - CELL_ARRAY_WIDTH - 1;
+                cell_add_neighbour(targ, &cell_arr[ni]);
+            }
+
+            /* Cell directly to the left */
+            ni = i - 1;
+            cell_add_neighbour(targ, &cell_arr[ni]);
+
+            /* Cell below to the left */
+            if (i < CELL_ARRAY_WIDTH * (CELL_ARRAY_HEIGHT-1)) {
+                ni = i + CELL_ARRAY_WIDTH - 1;
+                cell_add_neighbour(targ, &cell_arr[ni]);
+            }
+        }
+
         /* Column to the right */
         else if (i%CELL_ARRAY_WIDTH < CELL_ARRAY_WIDTH-1) {
+            /* Cell above to the right */
+            if (i >= CELL_ARRAY_WIDTH) {
+                ni = i - CELL_ARRAY_WIDTH + 1;
+                cell_add_neighbour(targ, &cell_arr[ni]);
+            }
 
-        }
-        /* Cell directly above */
-        else if (i >= CELL_ARRAY_WIDTH) {
+            /* Cell directly to the right */
+            ni = i + 1;
+            cell_add_neighbour(targ, &cell_arr[ni]);
 
-        }
-        /* Cell directly below */
-        else if (i < CELL_ARRAY_WIDTH * (CELL_ARRAY_HEIGHT-1)) {
-
+            /* Cell below to the right */
+            if (i < CELL_ARRAY_WIDTH * (CELL_ARRAY_HEIGHT-1)) {
+                ni = i + CELL_ARRAY_WIDTH + 1;
+                cell_add_neighbour(targ, &cell_arr[ni]);
+            }
         }
     }
 }
